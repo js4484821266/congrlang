1e882ab6455e604ca039de1e401f187e729b0cc0
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-09-10 01:37:10 +0900

    analyse syllables

--- eval.py
+++ eval.py
@@ -5,13 +5,36 @@ import congrlang
 def evaluate(lem: str) -> int:
     sum = 0  # sum
     wei = 1  # weight
-    vrx = re.compile(f'({"|".join(congrlang.VOWEL)})')  # vowels regex
-    syl = [re.search('','')]  # position of syllables
-    while syl[-1]:
-        syl.append(vrx.search(lem,pos=syl[-1].end()))
-    syl=syl[1:-1]
+    vrc = re.compile(
+        f'({"|".join(sorted(congrlang.VOWEL,key=lambda x:len(x),reverse=True))})',
+        re.I)  # vowels regex compiled
+    hrc = re.compile(
+        f'({"|".join(sorted([i for i in congrlang.HEAD if i],key=lambda x:len(x),reverse=True))})',
+        re.I)  # head consonants regex compiled
+    vwl = [re.search('', '')]  # position of syllables
+    while vwl[-1]:
+        # list of match object of vowels
+        vwl.append(vrc.search(lem, vwl[-1].end()))
+    if len(vwl) <= 2:
+        return 0
+    iii = 1
+    opn = []  # opening
+    clo = []  # closing
+    while vwl[iii]:  # filtering consonants
+        hhh = [vwl[iii-1]]  # list of consonants between vowels
+        while hhh[-1]:  # push consonants
+            hhh.append(hrc.search(lem, hhh[-1].end(), vwl[iii].start()))
+        iii += 1
+        try:
+            ttt = [i for i in hhh[1:-1] if i.group() in congrlang.TAIL]
+            clo.append(ttt[0]if hhh[-2].group() != ttt[0].group()else None)
+        except:
+            clo.append(None)
+        opn.append(hhh[-2])
+    clo.append(re.compile(f'({"|".join(i for i in congrlang.TAIL if i)})').search(
+        lem, vwl[-2].end()))
     return sum+1
 
 
 if __name__ == '__main__':
-    print(evaluate('11100a8288222     e   io  u555'))
+    print(evaluate('rarrrrrrrrarar'))
