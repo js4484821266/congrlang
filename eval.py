b23bff01dc4422fb8ad759dbf83506020b372bf0
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-09-09 15:59:42 +0900

    append

--- eval.py
+++ eval.py
@@ -4,9 +4,14 @@ import congrlang
 
 def evaluate(lem: str) -> int:
     sum = 0  # sum
-    
+    wei = 1  # weight
+    vrx = re.compile(f'({"|".join(congrlang.VOWEL)})')  # vowels regex
+    syl = [re.search('','')]  # position of syllables
+    while syl[-1]:
+        syl.append(vrx.search(lem,pos=syl[-1].end()))
+    syl=syl[1:-1]
     return sum+1
 
 
 if __name__ == '__main__':
-    print(evaluate('        m44488tchtcthccaaapaaaaiaiaiaaaaa884i   04022p'))
+    print(evaluate('11100a8288222     e   io  u555'))
