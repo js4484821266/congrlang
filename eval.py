83a0770d92949b6fecf44e9ac92ecbb869587aa1
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-09-12 17:22:30 +0900

    shorten

--- eval.py
+++ eval.py
@@ -12,42 +12,41 @@ def syllable_analysis(lem: str) -> "list[str]":
         f'({"|".join(sorted([i for i in congrlang.HEAD if i],key=len,reverse=True))})',
         re.I)
     # head consonants regex compiled
-    vowel = list(vrc.finditer(lem))
+    vwl = list(vrc.finditer(lem))
     # list of match object of vowels
-    if not vowel:
+    if not vwl:
         return []  # no vowels -> no syllables
 
     opn = []
     # opening consonant
     clo = []
     # closing consonant
-    for iii in range(len(vowel) + 1):
+    for iii in range(len(vwl) + 1):
         cons = []
         # list of consonants between vowels
         if iii == 0:
-            cons.extend(hrc.finditer(lem, 0, vowel[iii].start()))
-        elif iii == len(vowel):
-            cons.extend(hrc.finditer(lem, vowel[iii - 1].end(), len(lem)))
+            cons.extend(hrc.finditer(lem, 0, vwl[iii].start()))
+        elif iii == len(vwl):
+            cons.extend(hrc.finditer(lem, vwl[iii - 1].end(), len(lem)))
         else:
             cons.extend(hrc.finditer(
-                lem, vowel[iii - 1].end(), vowel[iii].start()))
+                lem, vwl[iii - 1].end(), vwl[iii].start()))
         # push consonants
 
-        tail = list(it.takewhile(lambda x: x.group() in congrlang.TAIL, cons))
-        # sieve closings
-        if len(cons) <= 1:
+        if len(cons) == 0:
             clo.append(None)
-        try:
-            opn.append(cons[-1])
-        except:
             opn.append(None)
-        if len(tail) >= 1:
+        elif len(cons) == 1:
+            clo.append(None)
+            opn.append(cons[0])
+        else:
             clo.append(cons[0])
+            opn.append(cons[-1])
 
     syl = []
-    for i in range(len(vowel)):
+    for i in range(len(vwl)):
         syl.append(('' if not opn[i] else opn[i].group()) +
-                   ('' if not vowel[i] else vowel[i].group()) +
+                   ('' if not vwl[i] else vwl[i].group()) +
                    ('' if not clo[i] else clo[i].group()))
     return syl
 
@@ -59,4 +58,4 @@ def evaluate(lem: str) -> int:
 
 
 if __name__ == '__main__':
-    print(evaluate('aioaioaioppppppppoppop'))
+    print(evaluate('anata no haato ni niko niko ni'))
