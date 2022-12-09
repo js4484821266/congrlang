530096952ef331103ab7d5e6179742ab79f8771d
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-10 06:23:41 +0900

    test eval

--- qwerty3v3c/eval.py
+++ qwerty3v3c/eval.py
@@ -1,3 +1,7 @@
+import re
 import cngr as C
+def eval(word:str):#->int:
+    w=re.sub('[^'+''.join(C.V+C.C)+']*','',word)
+    
 if __name__=='__main__':
-    print()
+    print(eval('cdsfcsdfscf22222cdfqqq'))
