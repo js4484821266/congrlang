5a0e560de495c2f3b750601b627cac7682677f98
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-10 20:42:05 +0900

    class Infl

--- infl.py
+++ infl.py
@@ -8,12 +8,38 @@ class Infl:
     Defines an object of an inflection.
     '''
 
-    def __init__(self, prim=1) -> None:
+    def __init__(self,
+                 prim=1,
+                 infl=1
+                 ) -> None:
         '''
-        Initializes attributes.
+        Initializes fields.
         `prim`: Primitive form of the word.
+        `infl`: Serial number of the inflection.
         '''
         self.prim = prim
+        self.infl = infl
+
+    def set(self,
+            prim=1,
+            infl=1
+            ) -> None:
+        '''
+        Initializes fields.
+        `prim`: Primitive form of the word.
+        `infl`: Serial number of the inflection.
+        '''
+        self.prim = prim
+        self.infl = infl
+
+
+class Sntx:
+    '''
+    Defines an object of a sentence.
+    '''
+
+    def __init__(self) -> None:
+        pass
 
 
 if __name__ == '__main__':
