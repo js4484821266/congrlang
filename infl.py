981e5e698927f9d8fed55723446fab509a39a1fe
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-10 23:45:16 +0900

    format infl.py

--- infl.py
+++ infl.py
@@ -1,10 +1,13 @@
 '''
 Universal inflector source code for every version of Congrlang.
 '''
+
+
 class Infl:
     '''
     Defines an object of an inflection.
     '''
+
     def __init__(self,
                  prim: int,
                  infl: int,
@@ -19,6 +22,7 @@ class Infl:
         self.prim = prim
         self.infl = infl
         self.nnnn = nnnn
+
     def set(self,
             prim: int,
             infl: int,
@@ -33,15 +37,19 @@ class Infl:
         self.prim = prim
         self.infl = infl
         self.nnnn = nnnn
+
     def out(self) -> int:
         '''
         Returns the inflection.
         '''
         return self.prim**self.infl % self.nnnn
+
+
 class Sntn:
     '''
     Defines an object of a sentence.
     '''
+
     def __init__(self,
                  subj: int,
                  verb: int,
@@ -56,11 +64,12 @@ class Sntn:
         self.subj = subj
         self.verb = verb
         self.advb = advb
+
     def set(self,
-                 subj: int,
-                 verb: int,
-                 advb: dict[int, int]
-                 ) -> None:
+            subj: int,
+            verb: int,
+            advb: dict[int, int]
+            ) -> None:
         '''
         Initializes fields.
         `subj`: The subject.
@@ -70,5 +79,7 @@ class Sntn:
         self.subj = subj
         self.verb = verb
         self.advb = advb
+
+
 if __name__ == '__main__':
     pass
