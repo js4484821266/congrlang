64ed4c281d168cab14dde98935516edc30c745a5
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-10 20:14:51 +0900

    docstring

--- infl.py
+++ infl.py
@@ -1,4 +1,20 @@
-class morpheme:
-    pass
-if __name__=='__main__':
+'''
+Universal inflector source code for every version of Congrlang.
+'''
+
+
+class Infl:
+    '''
+    Defines an object of an inflection.
+    '''
+
+    def __init__(self, prim=1) -> None:
+        '''
+        Initializes attributes.
+        `prim`: Primitive form of the word.
+        '''
+        self.prim = prim
+
+
+if __name__ == '__main__':
     pass
