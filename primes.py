57db293aaa16ce37d8e371517eb26595feda160c
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-11 11:52:38 +0900

    print i every 1000

--- primes.py
+++ primes.py
@@ -6,19 +6,20 @@ if __name__ == '__main__':
     with open('primes.txt', 'r', encoding='646') as f:
         p = sorted(list(map(int, f.read().split(','))))
     with open('primes.txt', 'w', encoding='646') as f:
-        i = p[-1]+2
+        i = p[-1]
+        print(i)
         try:
             while True:
+                i += 2
                 for j in p+[0]:
-                    if j > 1 and not (i % j):
+                    if j > 1 and not i % j:
                         break
                     elif not j:
                         p.append(i)
                     else:
                         pass
-                if not i % 1000:
+                if i % 1000 == 1:
                     print(i)
-                i += 2
         except KeyboardInterrupt:
             print(p[-1])
         f.write(','.join(map(str, p)))
