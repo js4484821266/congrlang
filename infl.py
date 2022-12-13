52ba0fead963c661233c3cb7493a01c75b5413d8
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-13 17:55:23 +0900

    pq mod p test

--- infl.py
+++ infl.py
@@ -14,9 +14,20 @@ def infl(prim: int, snum: int) -> int:
     if 1 >= prim or prim >= 2**20:
         raise ValueError("1 < prim < 2**20")
     if 0 >= snum or snum >= 2**11:
-        raise ValueError("1 < prim < 2**20")
+        raise ValueError("0 < prim < 2**11")
     return prim**snum % NNNN
 
 
 if __name__ == '__main__':
-    print(infl(1000, 500))
+    a = 6
+    b = 1
+    m = set()
+    l=0
+    while True:
+        n = infl(a, b)
+        print(f'{b}: {n}')
+        b+=1
+        l=len(m)
+        m.add(n)
+        if l==len(m):
+            break
