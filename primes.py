ba284ba4cc8278ac67ca430328bc95dc98f6f44c
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-14 22:45:24 +0900

    make the list of primes available

--- primes.py
+++ primes.py
@@ -3,13 +3,13 @@ primes
 '''
 import time
 p = []
+with open('primes.txt', 'r', encoding='646') as f:
+    p = sorted(list(map(int, f.read().split())))
 if __name__ == '__main__':
-    with open('primes.txt', 'r', encoding='646') as f:
-        p = sorted(list(map(int, f.read().split())))
     with open('primes.txt', 'w', encoding='646') as f:
         i = p[-1]
         print(i)
-        t000=time.time()
+        t000 = time.time()
         try:
             while True:
                 i += 2
@@ -21,9 +21,9 @@ if __name__ == '__main__':
                     else:
                         pass
                 if i % 1000 == 1:
-                    t001=time.time()
+                    t001 = time.time()
                     print(f'{i}: {t001-t000}s')
-                    t000=t001
+                    t000 = t001
         except KeyboardInterrupt:
             print(p[-1])
         f.write(' '.join(map(str, p)))
