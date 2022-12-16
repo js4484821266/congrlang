647e5af800e539a6cd0c27a34ad2cd420de81af8
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-16 21:28:40 +0900

    remove constant NNNN and limits

--- infl.py
+++ infl.py
@@ -18,22 +18,15 @@ Functions:
 - `infl(prim:int,snum:int)->int`: returns the inflection, `prim**snum%NNNN`
 '''
 import sys
-NNNN = 1073741827
-PRMX = 2**20
-SNMX = NNNN/PRMX
 
 
-def infl(prim: int, snum: int) -> int:
+def infl(prim: int, snum: int, nnnn: int) -> int:
     '''
     Initializes fields.
-    `1 < prim < PRMX`: Primitive form of the word.
-    `0 < snum < SNMX`: Serial number of the inflection.
+    `1 < prim`: Primitive form of the word.
+    `0 < snum`: Serial number of the inflection.
     '''
-    if 1 >= prim or prim >= PRMX:
-        raise ValueError(f'supposed to be 1 < prim < {PRMX}')
-    if 0 >= snum or snum >= SNMX:
-        raise ValueError(f'supposed to be 0 < snum < {SNMX}')
-    return prim**snum % NNNN
+    return prim**snum % nnnn
 
 
 if __name__ == '__main__':
@@ -42,7 +35,7 @@ if __name__ == '__main__':
     m = set()
     l = 0
     while True:
-        n = infl(a, b)
+        n = infl(a, b, 1073741827)
         print(f'{b}: {n}')
         b += 1
         l = len(m)
