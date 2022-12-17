910c374ac78363a4e1e376949f8b39bcf12a516e
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-17 23:56:53 +0900

    edit output interval & format

--- phi.py
+++ phi.py
@@ -1,6 +1,7 @@
 '''
 Euler's totient function.
 '''
+import math
 import time
 import primes
 
@@ -8,6 +9,7 @@ q = []
 with open('phi.txt', 'r', encoding='646') as f:
     q = list(map(int, f.read().split()))
 if __name__ == '__main__':
+    mlb = math.log(1.01)
     with open('phi.txt', 'w', encoding='646') as f:
         i = len(q)
         print(i)
@@ -20,19 +22,19 @@ if __name__ == '__main__':
                     if j*j > i:
                         break
                     t.append(0)
-                    while k>1 and not k % j:
+                    while k > 1 and not k % j:
                         t[-1] += 1
                         k //= j
                 u = 1
-                for pj,tj in zip(primes.p,t):
-                    v=pj**tj
-                    u*=v-v//pj
+                for pj, tj in zip(primes.p, t):
+                    v = pj**tj
+                    u *= v-v//pj
                 q.append(u)
-                if not i % 1000:
-                    t1 = time.time()
-                    print(f'{i}: {t1-t0}s')
+                t1 = time.time()
+                if t1-t0 >= 1:
+                    print(i)
                     t0 = t1
                 i += 1
         except KeyboardInterrupt:
             print(i)
-        f.write(' '.join(map(str, q)))
+        f.write('\n'.join(map(str, q)))
--- primes.py
+++ primes.py
@@ -7,25 +7,23 @@ p = []
 with open('primes.txt', 'r', encoding='646') as f:
     p = sorted(list(map(int, f.read().split())))
 if __name__ == '__main__':
-    ml1d=math.log(1.1)
+    mlb = math.log(1.01)
     with open('primes.txt', 'w', encoding='646') as f:
         i = p[-1]
         print(i)
-        t000 = time.time()
+        t0 = time.time()
         try:
             while True:
                 i += 2
                 for j in p+[0]:
                     if j > 1 and not i % j:
                         break
-                    elif not j:
+                    if not j:
                         p.append(i)
-                    else:
-                        pass
-                if int(math.log(i)/ml1d)>int(math.log(i-1)/ml1d):
-                    t001 = time.time()
-                    print(f'{i}: {int((t001-t000)*1000)}ms')
-                    t000 = t001
+                t1 = time.time()
+                if t1-t0 >= 1:
+                    print(i)
+                    t0 = t1
         except KeyboardInterrupt:
             print(p[-1])
         f.write(' '.join(map(str, p)))
