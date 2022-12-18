71f6bed5a8de84224830d510f0a5879feb155e73
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-18 13:06:41 +0900

    alter output format

--- primes.py
+++ primes.py
@@ -1,14 +1,12 @@
 '''
 primes
 '''
-import math
 import time
 p = []
 with open('primes.txt', 'r', encoding='646') as f:
-    p = sorted(list(map(int, f.read().split())))
+    p = list(map(int, f.read().split()))
 if __name__ == '__main__':
-    mlb = math.log(1.01)
-    with open('primes.txt', 'w', encoding='646') as f:
+    with open('primes.txt', 'a', encoding='646') as f:
         i = p[-1]
         print(i)
         t0 = time.time()
@@ -19,11 +17,10 @@ if __name__ == '__main__':
                     if j > 1 and not i % j:
                         break
                     if not j:
-                        p.append(i)
+                        f.write(f'\n{i}')
                 t1 = time.time()
                 if t1-t0 >= 1:
                     print(i)
                     t0 = t1
         except KeyboardInterrupt:
-            print(p[-1])
-        f.write('\n'.join(map(str, p)))
+            print(i)
