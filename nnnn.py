fd016c0889e92a7b8b0b4c5734619461ecd90b24
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-18 22:38:10 +0900

    nnnn.py

--- nnnn.py
+++ nnnn.py
@@ -1,5 +1,26 @@
 '''
 helps designate N.
 '''
+import primes
 import phi
-N = 2
+N = 0
+M = 0
+r = len(primes.p)
+p = ()
+for i in primes.p:
+    try:
+        j = phi.q[i-1]/phi.q[i]
+        k = phi.q[i+1]/phi.q[i]
+        if j < r:
+            N = i
+            M = i-1
+            r = j
+        if k < r:
+            N = i
+            M = i+1
+            r = k
+        p=(N, M, r)
+    except IndexError:
+        break
+if __name__ == '__main__':
+    print(p)
