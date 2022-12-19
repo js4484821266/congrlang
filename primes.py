9ae6f3fe2d22e6576859c99566d40a29a233f3f4
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-20 06:04:16 +0900

    modularize for multiprocessing

--- primes.py
+++ primes.py
@@ -3,24 +3,40 @@ primes
 '''
 import time
 p = []
-with open('primes.txt', 'r', encoding='646') as f:
-    p = list(map(int, f.read().split()))
+with open('primes.txt', 'r', encoding='646') as r:
+    p = list(map(int, r.read().split()))
+
+
+def nxpr(inon: int = p[-1]):
+    '''
+    the next prime?
+    - `inon`: initial odd number
+    '''
+    while True:
+        inon += 2
+        dlep=0
+        for jjjj in p+[0]:
+            if jjjj > 1 and not inon % jjjj:
+                break
+            if not jjjj:
+                p.append(inon)
+                dlep=1
+        if dlep:
+            break
+        
+
+
 if __name__ == '__main__':
-    with open('primes.txt', 'a', encoding='646') as f:
-        i = p[-1]
-        print(i)
-        t0 = time.time()
-        try:
-            while True:
-                i += 2
-                for j in p+[0]:
-                    if j > 1 and not i % j:
-                        break
-                    if not j:
-                        f.write(f'\n{i}')
-                t1 = time.time()
-                if t1-t0 >= 1:
-                    print(i)
-                    t0 = t1
-        except KeyboardInterrupt:
-            print(i)
+    print(p[-1])
+    t0 = time.time()
+    try:
+        # while True:
+        nxpr()
+        t1 = time.time()
+        if t1-t0 >= 1:
+            print(p[-1])
+            t0 = t1
+    except KeyboardInterrupt:
+        print(p[-1])
+    with open('primes.txt', 'w', encoding='646') as w:
+        w.write('\n'.join(map(str, sorted(set(p)))))
