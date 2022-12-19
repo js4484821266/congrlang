45a12e6545e8de5c225d7f934e936c019cc11cea
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-20 06:12:06 +0900

    TODO

--- nnnn.py
+++ nnnn.py
@@ -4,23 +4,15 @@ Designates N.
 import primes
 import phi
 N = 0
-M = 0
-r = len(primes.p)
-p = ()
-for i in primes.p:
+r = 9
+for i in primes.p[1:]:
     try:
-        j = phi.q[i-1]/phi.q[i]
-        k = phi.q[i+1]/phi.q[i]
-        if j < r:
+        j = phi.q[i]/phi.q[phi.q[i]]
+        if j <= r:
             N = i
-            M = i-1
             r = j
-        if k < r:
-            N = i
-            M = i+1
-            r = k
-        p=(N, M, r)
+            # TODO
     except IndexError:
         break
 if __name__ == '__main__':
-    print(p)
+    print(N, r)
