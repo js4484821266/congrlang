2a9d23c4555c4eada5f650133aabcda7c676b028
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-20 16:36:13 +0900

    remove argument

--- primes.py
+++ primes.py
@@ -6,25 +6,27 @@ with open('primes.txt', 'r', encoding='646') as r:
     p = list(map(int, r.read().split()))
 
 
-def nxpr(inon: int = p[-1]):
+def nxpr():
     '''
     the next prime?
-    - `inon`: initial odd number
     '''
     while True:
+        inon = p[-1]
         inon += 2
         for jjjj in p+[0]:
+            if jjjj**2 > inon:
+                break
             if jjjj > 1 and not inon % jjjj:
                 break
             if not jjjj:
                 return inon
 
 
-def papp(inon: int = p[-1])->None:
+def papp() -> None:
     '''
     appends nxpr to p.
     '''
-    prim = nxpr(inon)
+    prim = nxpr()
     if prim in p:
         return
     else:
@@ -34,24 +36,26 @@ def papp(inon: int = p[-1])->None:
 if __name__ == '__main__':
     print(p[-1])
     import threading
+    nthr = 8
     thrs = list()
     try:
-        for i in range(8):
-            t=threading.Thread(target=papp,daemon=True)
+        for i in range(nthr):
+            t = threading.Thread(target=papp, daemon=True)
             t.start()
             thrs.append(t)
         while True:
-            for t in thrs:
-                if t.is_alive():
+            for t in range(nthr):
+                if thrs[t].is_alive():
                     continue
-                t.join()
-                t=threading.Thread(target=papp)
-                t.start()
+                thrs[t].join()
+                print(f'Thread {t}... {p[-1]}')
+                thrs[t] = threading.Thread(target=papp)
+                thrs[t].start()
     except KeyboardInterrupt:
         print("KeyboardInterrupt")
         for t in thrs:
             t.join()
-    P=sorted(set(p))
+    P = sorted(set(p))
     print(P[-1])
     with open('primes.txt', 'w', encoding='646') as w:
         w.write('\n'.join(map(str, P)))
