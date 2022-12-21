e5aa6f30cdab26aa7af95e661fa852a577a2cc75
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-21 23:10:36 +0900

    TODO

--- nnnn.py
+++ nnnn.py
@@ -1,18 +1,11 @@
 '''
 Designates N.
 '''
-import primes
 import phi
 N = 0
-r = 9
-for i in primes.p[1:]:
-    try:
-        j = phi.q[i]/phi.q[phi.q[i]]
-        if j <= r:
-            N = i
-            r = j
-            # TODO
-    except IndexError:
-        break
+R = 9
+for i, j in phi.q:
+    # TODO
+    pass
 if __name__ == '__main__':
-    print(N, r)
+    print(N, R)
