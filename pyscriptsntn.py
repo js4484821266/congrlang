fc827d2dc7261128f25330bae5973d904a044b98
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2022-12-26 23:02:54 +0900

    fix error

--- pyscriptsntn.py
+++ pyscriptsntn.py
@@ -1,17 +1,19 @@
 from pyscript import Element
+
+
 def sntn():
-    xxxx=0
-    dddd=dict()
+    xxxx = 0
+    dddd = dict()
     idid = ['vbab', 'vbnn', 'erab', 'ernn', 'abab', 'abnn']
     for i in idid:
-        dddd[i] = int(Element(i))
+        dddd[i] = int(Element(i).element.value)
     mmmm = 1
     for id in idid[1::2]:
         mmmm *= dddd[id]
-    for id,ab in zip(idid[1::2],idid[::2]):
-        n=mmmm//dddd[id]%dddd[id]
-        s=1
-        while n*s%dddd[id]!=1:
-            s+=1
-        xxxx+=ab*n*s
+    for id, ab in zip(idid[1::2], idid[::2]):
+        n = mmmm//dddd[id] % dddd[id]
+        s = 1
+        while n*s % dddd[id] != 1:
+            s += 1
+        xxxx += ab*n*s
     Element('sntn').write(xxxx)
